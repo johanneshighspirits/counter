b560999a37000eb4497722e59f8ec045427b7244
Johannes Borgström <johannes@highspirits.se>
2026-01-17 21:58:35 +0100

    title update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Anton, Bebas_Neue, Geist, Geist_Mono, Oswald } from 'next/font/google';
+import { Bebas_Neue, Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
 const geistSans = Geist({
@@ -12,12 +12,6 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
-const oswald = Oswald({ variable: '--font-oswald', subsets: ['latin'] });
-const anton = Anton({
-  variable: '--font-anton',
-  subsets: ['latin'],
-  weight: ['400'],
-});
 const bebasNeue = Bebas_Neue({
   variable: '--font-bebas',
   weight: ['400'],
@@ -25,8 +19,8 @@ const bebasNeue = Bebas_Neue({
 });
 
 export const metadata: Metadata = {
-  title: 'Event Visitor Counter',
-  description: 'Real-time visitor counter with Supabase',
+  title: 'Hammarby Hockey | Team 17',
+  description: 'Real-time visitor counter',
 };
 
 export default function RootLayout({
@@ -40,8 +34,6 @@ export default function RootLayout({
         className={[
           geistSans.variable,
           geistMono.variable,
-          oswald.variable,
-          anton.variable,
           bebasNeue.variable,
           'antialiased',
         ].join(' ')}>
